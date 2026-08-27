8fbfa5b5625eefa7dca8a21119eeb6e6ac15ec3d
amrit1610-fin <amritanshuofficial718@gmail.com>
2026-08-27 14:47:23 +0530

    Drop deep OTM options

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from data.data_cleaning import CleanedOptionChain
 from utils.implied_vol import ImpliedVolatility
 
+# VARIABLES
 data_folder_path = "data/option_chains"
 spot = 24281.30
 rate = 0.07
 
+# OPTIONS CHAIN DATA
 data_engine = CleanedOptionChain(data_folder_path, spot, rate)
 data = data_engine.process_all()
 
+# ADDING CALCULATED IMPLIED VOLATILITY
 iv_engine = ImpliedVolatility()
-
 data['iv_calc'] = data.apply(iv_engine.implement_implied_vol, axis=1)
+data.dropna(subset=['iv_calc'], inplace=True)                                # Dropping rows where IV calculation failed (deep OTM or stale quotes)
+
 
 print(data.head())
 print(data.shape)
